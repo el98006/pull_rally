b3c338a7a005de7ea81cbffc19d721fca0216bcb
el98006 <el98006@gmail.com>
2017-02-27 13:40:56 -0800

    switched to requests library

--- src/pull_rally.py
+++ src/pull_rally.py
@@ -8,9 +8,15 @@ import xml.etree.ElementTree as xt
 import os
 import urllib2
 import json
+import multiprocessing
+import logging
+from utils import retry_on_exception, init_logging
+import requests
+from requests.auth import AuthBase, HTTPBasicAuth
+
 
 ROOT_DIR  = os.path.expanduser('~/Downloads/rally_dump')
-    
+RETRY_NUM = 5    
     
 INDEX_PAGE_HEADER = '<!DOCTYPE html> \
     <html><head><title>Platform Team User Stories </title> <link rel="stylesheet" href="css/bootstrap.min.css"> </head>\
@@ -22,11 +28,13 @@ INDEX_PAGE_FOOTER = '</tbody> </table> </body> </html>'
     
 XML_FILENAME = 'Rally-Stories.xml'
 
+BASE_URL = 
+UNAME = 
+PASS = 
 
+LINE_CAP = None
 
-LINE_CAP = 30
-
-SIMPLE_ELEMENT = ['ObjectID','FormattedID','Name','Description', 'Notes','DirectChildrenCount', 'HasParent', 'c_ECommKanbanState', 'c_ReleaseDate','c_RT','c_ReleasePlan']
+SIMPLE_ELEMENT = ['ObjectID','FormattedID','Name','Description', 'Notes','DirectChildrenCount', 'HasParent', 'c_ECommKanbanState', 'c_ReleaseDate','c_RT','c_ReleasePlan','Project']
 ARRAY_ELEMENT = ['Defects',  'Discussion', 'Tasks', 'TestCases', 'Attachments','Projects', 'Children', 'TestCases', 'Tags']
     
 INDEX_PAGE_REC_TEMPLATE ='<tr> <td> <a href="{}"> {}</a> </td> <td>{}</td> <td>{}</td> <td>{}</td> </tr>'
@@ -37,8 +45,8 @@ DETAIL_PAGE_STYLE = '<style> panel panel_default { padding-left: 80px; backgroun
 DETAIL_PAGE_FOOTER_TEMPLATE = '</body> </html>'
 
 DETAIL_PAGE_SECTION_TEMPLATE = '<h3> {heading} </h3> <div class="panel panel-default"> <div class="panel-body"> {body} </div> </div>'
-SECTION_IN_SEQUENCE = [ 'Description', 'Notes', 'Attachments', 'Tasks',  'Defects', 'Discussion', 'Tags', 'Owner', 'c_RT', 'c_ECommKanbanState', 'c_ReleaseDate','c_ReleasePlan', 'TestCase']
-DETAIL_BODY_TEMPLATE = {'Defects':'<li> <a href="{}" > {} </a> {} </li>', 'Discussion':'<li> <p class="text-muted"> <small>{} </small> at {} </p> <p> {} </p> </li>',\
+SECTION_IN_SEQUENCE = [ 'Description', 'Notes', 'Attachments', 'Tasks',  'Defects', 'Discussion', 'Tags', 'Owner', 'c_RT', 'c_ECommKanbanState', 'c_ReleaseDate','c_ReleasePlan', 'TestCase', 'Project']
+DETAIL_BODY_TEMPLATE = {'Defects':'<li> <a href="{}" > {} </a> {} </li>', 'Discussion':'<li> <p class="text-muted"> <small>{} at {} </small></p> <p> {} </p> </li>',\
                  'Tasks':'<li> <a href="{}" > {} </a> {} </li>', 'Attachments':'<li> <a href="{}"> {} </a> </li>'}
 HEADING_MAPPING = {'c_ECommKanbanState':'State', 'c_ReleaseDate':'ReleaseDate', 'c_RT':'RT', 'c_ReleasePlan':'ReleasePlan'}
 
@@ -50,31 +58,38 @@ def non_blank_element(content):
         return False
     else:
         return True
-
+    
+    
+@retry_on_exception
 def authenticate_http():
     passwd_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
     passwd_mgr.add_password(None, BASE_URL, UNAME, PASS)
     handler = urllib2.HTTPBasicAuthHandler(passwd_mgr)
     opener = urllib2.build_opener(handler)
-    opener.addheaders = [('Accepted', 'text/xml')]
+    #opener.addheaders = [('Accepted', 'text/xml')]
     urllib2.install_opener(opener)
     '''install the opener, so all the future urllib.urlopen urlib2.request will base on this opener'''
 
+def sanitize_text( line):
+    result = re.sub('"', '&quot',line)
+    result = result.encode('utf-8')
+    return result
 
 
 def wrap_section_body_in_html(heading, details):
 #heading, SECTION_SERIES
 #details array of tuples
-    
-    
+    #logger = init_logging(logging.INFO, 'wrap_section_body_in_html')     
     #create unordered list if body is an array
     if heading is 'Tags':
         html_result = details
     elif heading in ARRAY_ELEMENT:
         result  = ['<ul>']
         for line in details:
-            html_line = DETAIL_BODY_TEMPLATE[heading].format(*line)
-            result.append(html_line)    
+            print line
+            if line <> None:
+                html_line = DETAIL_BODY_TEMPLATE[heading].format(*line)
+                result.append(html_line)    
         result.append('</ul>')
         html_result = ''.join(result)
     else:
@@ -82,95 +97,119 @@ def wrap_section_body_in_html(heading, details):
     return html_result      
 
 
+def get_conversation_by_url(url):    
+        auth = HTTPBasicAuth(username=UNAME,password=PASS )
+        r = requests.get(url, auth=auth, timeout = 5)
+        if r.status_code == '200':
+            root = r.json()
+            p_text = root.get('ConversationPost').get('Text')
+            p_user = root.get('ConversationPost').get('User').get('_refObjectName')
+            p_timestamp = root.get('ConversationPost').get('CreationDate')
+            return  p_user, p_timestamp, sanitize_text(p_text)  
+        else:
+            my_logger.log(logging.INFO, 'error {} getting conversation detail from {}'.format(r.status_code, url))
+            return None 
 
-def download_conversation_details(gen_urls):
-    for url in gen_urls:
-        authenticate_http()
-        ret = urllib2.urlopen(url)
-        resp = ret.read()
-        root = json.loads(resp)
-        p_text = root.get('ConversationPost').get('Text')
-        p_user = root.get('ConversationPost').get('User').get('_refObjectName')
-        p_timestamp = root.get('ConversationPost').get('CreationDate') 
-        yield  p_user, p_timestamp, p_text,
-        
-
+  
+def get_task_by_url(url):
+        auth = HTTPBasicAuth(username=UNAME,password=PASS )
+        r = requests.get(url, auth=auth, timeout = 5)
+        if r.status_code <> '200':
+            my_logger.log(logging.INFO, 'error {} getting task detail from {}'.format(r.status_code, url))
+            return None
+        else:
+            root = r.json()
+            ta_id = root.get('Task').get('ObjectID')
+            ta_name= root.get('Task').get('FormattedID')
+            ta_desc = root.get('Task').get('Name')
+            link = ''.join(['./Task/', str(ta_id), '.html'])   
+            my_logger.log(logging.INFO, 'task detail: {}'.format(sanitize_text(ta_name)))
+            return link, sanitize_text(ta_name), sanitize_text(ta_desc)   
     
-def download_task_details(gen_url=None):
     
-    for url in gen_url:
-        authenticate_http()
-        ret = urllib2.urlopen(url)
-        resp = ret.read()
-        root = json.loads(resp)
-        ta_id = root.get('Task').get('ObjectID')
-        ta_name= root.get('Task').get('FormattedID')
-        ta_desc = root.get('Task').get('Name')
-        link = os. path.join('./Task', str(ta_id), '.html')       
-        yield link, ta_name, ta_desc    
 
-
-
-def download_attachment_details(detail_urls):
+def get_attachment_by_url(url):
+    
+    retry_count = 1
+    
     
     t_path = os.path.join(ROOT_DIR,'Attachment')
-    if os.path.exists(t_path) is False:
-        os.mkdir(t_path)
-
     
-    for url in detail_urls:
-        authenticate_http()
-        ret = urllib2.urlopen(url)
-        resp = ret.read()
-        root = json.loads(resp)
-        t_id = root.get('Attachment').get('ObjectID')
-        t_content_url = root.get('Attachment').get('Content').get('_ref')
-        t_content_name = unicode(root.get('Attachment').get('Name'))
+    if os.path.exists(t_path) is False:
+        try:
+            os.mkdir(t_path)
+        except OSError as e:
+            my_logger.log('failed to mkdir {}, error{}'.format(t_path, e))
+            
+    auth = HTTPBasicAuth(username=UNAME, password=PASS)
+    ret = requests.get(url, auth=auth, timeout=5)
+    
+    while ret.status_code <> '200' and retry_count < 5:
+        ret = requests.get(url, auth=auth, timeout=5)
+        retry_count += 1
+    root = ret.json()
+    t_id = root.get('Attachment').get('ObjectID')
+    t_content_url = root.get('Attachment').get('Content').get('_ref')
+    t_content_name = unicode(root.get('Attachment').get('Name'))
        
         
-        t_path = os.path.join(t_path, str(t_id))
+    t_fullpath = os.path.join(t_path, str(t_id))
         
-        # t_fullpath is used to download attachments.  
-        if os.path.exists(t_path) is False:
-            os.mkdir(t_path)
-        t_fullpath = os.path.join(t_path, t_content_name)       
+    # t_fullpath is used to download attachments.  
+    if os.path.exists(t_fullpath) is False:
+        try:
+            os.mkdir(t_fullpath)
+        except:
+            my_logger.log('failed to create t_fullpath {}'.format(t_fullpath))
+     
+            
+    t_fullpath = os.path.join(t_fullpath, t_content_name)       
         
         # download the attachment file using t_content_url, create file with the original filename
-        req = urllib2.Request(t_content_url, headers={'User-Agent':'Mozilla/5.0'}, data=None)
-        response = urllib2.urlopen(req)
-        root = json.loads(response.read()) 
-        
-        with open(t_fullpath, 'wb') as fh:
-            payload = root.get('AttachmentContent').get('Content')
-            payload = payload.decode('base64')
-            fh.write(payload)
+    ret = requests.get(t_content_url, auth=auth, headers={'User-Agent':'Mozilla/5.0'})
+    root = ret.json()
+
+    with open(t_fullpath, 'wb') as fh:
+        payload = root.get('AttachmentContent').get('Content')
+        payload = payload.decode('base64')
+        fh.write(payload)
         
         # t_link is used to create link on the user story detailed page, use relative url 
-        t_link = os.path.join('./Attachment', str(t_id), t_content_name)
-        
-        yield  t_link.encode('utf-8'), t_content_name.encode('utf-8') 
-        
+    t_link = os.path.join('./Attachment', str(t_id), t_content_name)
+    my_logger.log(logging.INFO, 'attachment created for {} {}'.format(t_id, t_content_name)) 
+    return  t_link.encode('utf-8'), t_content_name.encode('utf-8')  
 
-
-def download_defect_details(gen_url):
     
-    for url in gen_url:
+        
+@retry_on_exception
+def get_defect_by_url(url):
+    # using laborous native urllib2 method, just for comparison. requests library is more convenient.
+    
         authenticate_http()
         ret = urllib2.urlopen(url)
         resp = ret.read()
-        root = json.loads(resp)
-        de_id = root.get('Defect').get('ObjectID')
-        de_name= root.get('Defect').get('FormattedID')
-        de_desc = root.get('Defect').get('Name')
-        link = os. path.join('./Defect', str(de_id), '.html')       
-        yield link, de_name, de_desc
-
+        if resp <> None:
+            root = json.loads(resp)
+        
+            de_id = root.get('Defect').get('ObjectID')
+            de_name= root.get('Defect').get('FormattedID')
+            de_desc = root.get('Defect').get('Name')
+            link = ''.join(['./Defect/', str(de_id), '.html'])
+            my_logger.log(logging.INFO, 'defect detail: {}'.format(de_name))
+            return link, de_name, sanitize_text(de_desc)
+        else:
+            my_logger.log(logging.INFO, 'error getting defect {}'.format(url))
+            return None
+    
 def extract_from_itemarray(xml_element):
     for item in xml_element.find('_itemRefArray'):
-        url_ref = item.attrib['ref']
-        yield url_ref
-
-
+        try: 
+            url_ref = item.attrib['ref']
+            yield url_ref
+        except KeyError:
+            my_logger.log(logging.INFO, 'ref doesn\'t exist in _itemRefArray')
+        except:
+            pass
 
 def get_tags(xml_element):
     tag_list =[]
@@ -189,9 +228,10 @@ def get_tags(xml_element):
         return None
     
 def commmon_array_handler(xml_element):
-    SECTION_HANDLING_FUNCTIONS = {'Discussion':download_conversation_details, 'Attachments':download_attachment_details, \
-    'Defects':download_defect_details, 'Tasks':download_task_details}
-
+    SECTION_HANDLING_FUNCTIONS = {'Discussion':get_conversation_by_url, 'Attachments':get_attachment_by_url, \
+    'Defects':get_defect_by_url, 'Tasks':get_task_by_url} 
+    tuple_list =[]
+    
     element_type = xml_element.tag
     
     if element_type == 'Tags':
@@ -199,20 +239,29 @@ def commmon_array_handler(xml_element):
         return tags
     else:
         generator_urls = extract_from_itemarray(xml_element)
-        generator_tuples = SECTION_HANDLING_FUNCTIONS[element_type](generator_urls)
-    return generator_tuples
+        for url in generator_urls:
+            val = SECTION_HANDLING_FUNCTIONS[element_type](url)
+            if val <> None:
+                tuple_list.append(val)
+    
+        return tuple_list
 
 
 def get_story_details(us_element):
-     
+    
+    my_logger = init_logging(logging.INFO, 'get_story_details')
     details = {}
+    
+    
+    authenticate_http()
+    
     for c in us_element.getchildren():
-        if c.tag == 'Owner' and c.attrib['refObjectName']:
-            details[c.tag] = c.attrib['refObjectName']
+        if c.tag in ['Owner', 'Project'] and c.attrib['refObjectName']:
+            details[c.tag] = sanitize_text(unicode(c.attrib['refObjectName']))
             
         if c.tag in SIMPLE_ELEMENT:
             if non_blank_element(c.text):
-                details[c.tag] = c.text
+                details[c.tag] = sanitize_text(c.text)
         elif c.tag in  ARRAY_ELEMENT:
          
             try:
@@ -222,6 +271,7 @@ def get_story_details(us_element):
                 elif child_num.text <> '0':
                     # if have of list of items stored in _itemRefArray, based on the element type, route to handler functions.  
                     details[c.tag] = commmon_array_handler(c)
+                    my_logger.log(logging.INFO, 'tag: {} | {}'.format(c.tag, details[c.tag]))
             except KeyError:
                 pass
 
@@ -241,7 +291,7 @@ def generate_detail_page_section(fh, details):
             section_html = DETAIL_PAGE_SECTION_TEMPLATE.format(heading=heading, body=body)          
             fh.writelines(section_html)
         except KeyError:
-            print 'heading doesn\'t exist {}, skipping'.format(heading)
+            my_logger.log(logging.INFO, 'heading doesn\'t exist {}, skipping'.format(heading) )
             continue
 
 
@@ -273,21 +323,28 @@ def process_xml(fullpath, indexf):
     output_dir = os.path.dirname(fullpath)
     
     for i in xf.getroot().getchildren():
-        if lc >= LINE_CAP:
+        if LINE_CAP is not None and lc >= LINE_CAP:
             break
         else:
+            # get_story_details has the core parsing logics
             details = get_story_details(i)
-            detail_page_name = './' + details['ObjectID'] + '.html'
-            line =  INDEX_PAGE_REC_TEMPLATE.format(detail_page_name, details['FormattedID'], i.attrib['refObjectName'], details['c_ECommKanbanState'], i.attrib['CreatedAt'])
-            indexf.writelines(line)
-            print line
-            
+            try:
+                detail_page_name = './' + details['ObjectID'] + '.html'
+                line =  INDEX_PAGE_REC_TEMPLATE.format(detail_page_name, details['FormattedID'], sanitize_text(i.attrib['refObjectName']), sanitize_text(details['c_ECommKanbanState']), i.attrib['CreatedAt'])
+                indexf.writelines(line)
+                my_logger.log(logging.INFO,'started processing {}'.format(details['FormattedID']) )
+            except KeyError:
+                my_logger.log(logging.INFO, 'KeyError in process_xml')
+                continue    
             generate_detail_page(details, output_dir)
             
             lc += 1
+            print 'processing {}'.format(str(lc))
 
 if __name__ == '__main__':
-
+    
+    my_logger = init_logging(level=logging.INFO)
+    
     index_file = open(os.path.join(ROOT_DIR, 'index.html'),'wt')
     index_file.write(INDEX_PAGE_HEADER)
     xml_path = os.path.join(ROOT_DIR, XML_FILENAME)
---
+++ src/utils.py
@@ -0,0 +1,62 @@
+'''
+Created on Feb 16, 2017
+
+@author: eli
+'''
+
+import time
+from functools import wraps
+import logging
+import urllib2
+
+
+MAX_RETRIES = 5
+POOL_SIZE = 5
+
+def retry_on_exception(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        my_logger = init_logging(logging.INFO,'root')
+        num_retries = 0
+        ret = None 
+        while num_retries < MAX_RETRIES:
+            try:
+                ret = func(*args, **kwargs)
+                break
+            except urllib2.HTTPError as e:
+                if num_retries < MAX_RETRIES:
+                    time.sleep(50)
+                    ret = func(*args, **kwargs)
+                    num_retries += 1
+                else:
+                    print 'maxed out HTTPError retries'
+            except urllib2.URLError as e:
+                if num_retries < MAX_RETRIES:
+                    time.sleep(50)
+                    ret = func(*args, **kwargs)
+                    my_logger(logging.INFO, 'retry exception {}'.format(e.code))
+                else:
+                    print 'maxed out retries'
+                num_retries += 1    
+            except:
+                print "unhandled error" 
+                my_logger.log(logging.INFO, 'unhandled error')  
+                num_retries += 1 
+                 
+        return ret
+    
+    return wrapper 
+
+def init_logging(level=logging.ERROR, mod_name = 'root'):
+    logger = logging.getLogger(mod_name)
+    logger.setLevel(level)
+    
+    fh = logging.FileHandler('pull_rally.log')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    #formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
+    
+
+        
